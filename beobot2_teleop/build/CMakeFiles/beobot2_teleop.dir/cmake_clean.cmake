6944051fb98c56df5d02bc8d75e1a688294ccde7
mezlxx@gmail.com <mezlxx@gmail.com@7f7be7a4-2c22-d3cd-29bd-1377c3b46954>
2010-09-27 20:30:30 +0000

    finish basic beobot navigation stack

---
+++ beobot2_teleop/build/CMakeFiles/beobot2_teleop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/beobot2_teleop.dir/src/beobot2_teleop.o"
+  "../bin/beobot2_teleop.pdb"
+  "../bin/beobot2_teleop"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/beobot2_teleop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- beopilot/src/app-BeoPilot.cpp
+++ beopilot/src/app-BeoPilot.cpp
@@ -24,83 +24,126 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-// %Tag(FULLTEXT)%
-// %Tag(ROS_HEADER)%
 #include "ros/ros.h"
-// %EndTag(ROS_HEADER)%
-// %Tag(MSG_HEADER)%
 #include "std_msgs/String.h"
 #include "BeoPilot.H"
 #include <geometry_msgs/Twist.h>
-// %EndTag(MSG_HEADER)%
-
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
 #include <sstream>
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
+//joy -> joy/Joy.msg -> teleop -> geometry_msgs/Twist.msg -> this
 
 BeoPilot * beopilot;
-void motorCallback(geometry_msgs::Twist msg)
+void cmd_vel_Callback(geometry_msgs::Twist msg)
 {
 	float trans = msg.linear.x;//Trans
 	float rots = msg.angular.z;//Rot
-	beopilot->SetMotors(rots,trans);
+	ROS_INFO("cmd_vel_callback: %f, %f", trans, rots);
+	beopilot->SetMotorsPid(rots,trans);
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "beopilot");
-  beopilot = new BeoPilot();
-  beopilot->start();
+  	ros::init(argc, argv, "beopilot");
+  	ros::NodeHandle n;
+  	ros::NodeHandle n_priv("~");
+	ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("/beobot2/odom", 1);
+	tf::TransformBroadcaster odom_broadcaster;
+	beopilot = new BeoPilot();
+	beopilot->start();
+	ros::Subscriber motor_sub = n.subscribe("/beobot2/cmd_vel",1,cmd_vel_Callback);
 
-  ros::NodeHandle n;
+	ros::Rate pub_rate(10);
+	double x = 0.0f;
+	double y = 0.0f;
+	double th = 0.0f;
 
-  ROS_INFO("subscribing to motor command");
-  ros::Subscriber motor_sub = n.subscribe("/base_controller/command",1,motorCallback);
-//  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
-//  tf::TransformBroadcaster odom_broadcaster;
+	double vx = 0.0f;
+	double vy = 0.0f;
+	double vth = 0.0f;
+
+	ros::Time current_time, last_time;
+	current_time = ros::Time::now();
+	last_time = ros::Time::now();
+	
+	beopilot->resetEncoder();
+	ROS_INFO("x %f y %f th %f vx %f vy %f vth %f", beopilot->itsPosition.x, beopilot->itsPosition.y, beopilot->itsPosition.z, beopilot->itsVelocity.x, beopilot->itsVelocity.y, beopilot->itsVelocity.z);
+	
+	while (ros::ok())
+	{
+		beopilot->UpdateRCStatus();
+		x = -beopilot->itsPosition.x;
+		y = beopilot->itsPosition.y;
+		th = -beopilot->itsPosition.z;
+		
+		vx = -beopilot->itsVelocity.x;
+		vy = beopilot->itsVelocity.y;
+		vth = -beopilot->itsVelocity.z;
+		
+		ROS_INFO("x %f y %f th %f vx %f vy %f vth %f", x, y, th, vx, vy, vth);
+		
+		//update velocities here
+		
+		current_time = ros::Time::now();
+
+		//compute odometry in a typical way given the velocities of the robot
+		//double dt = (current_time - last_time).toSec();
+		//double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
+		//double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
+		//double delta_th = vth * dt;
+
+		//x += delta_x;
+		//y += delta_y;
+		//th += delta_th;
+
+		//since all odometry is 6DOF we'll need a quaternion created from yaw
+		geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
+
+		//first, we'll publish the transform over tf
+		geometry_msgs::TransformStamped odom_trans;
+		odom_trans.header.stamp = current_time;
+		odom_trans.header.frame_id = "/beobot2/odom";
+		odom_trans.child_frame_id = "/beobot2/base_link";
+
+		odom_trans.transform.translation.x = x;
+		odom_trans.transform.translation.y = y;
+		odom_trans.transform.translation.z = 0.0f;
+		odom_trans.transform.rotation = odom_quat;
+
+		//send the transform
+		odom_broadcaster.sendTransform(odom_trans);
+
+		//next, we'll publish the odometry message over ROS
+		nav_msgs::Odometry odom;
+		odom.header.stamp = current_time;
+		odom.header.frame_id = "odom";
+
+		//set the position
+		odom.pose.pose.position.x = x;
+		odom.pose.pose.position.y = y;
+		odom.pose.pose.position.z = 0.0f;
+		odom.pose.pose.orientation = odom_quat;
+
+		//set the velocity
+		odom.child_frame_id = "base_link";
+		odom.twist.twist.linear.x = vx;
+		odom.twist.twist.linear.y = vy;
+		odom.twist.twist.angular.z = vth;
+
+		//publish the message
+		odom_pub.publish(odom);
+
+		last_time = current_time;
+		ros::spinOnce();
+		pub_rate.sleep();
+	}
 
 
-  ros::Rate loop_rate(10);
-
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-// %Tag(ROS_OK)%
-  int count = 0;
-  while (ros::ok())
-  {
-// %EndTag(ROS_OK)%
-// %Tag(FILL_MESSAGE)%
-    std_msgs::String msg;
-
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-
-    beopilot->UpdateRCStatus();
-// %EndTag(FILL_MESSAGE)%
-
-// %Tag(ROSCONSOLE)%
-//    ROS_INFO("%s", msg.data.c_str());
-// %EndTag(ROSCONSOLE)%
-
-// %Tag(PUBLISH)%
-    chatter_pub.publish(msg);
-// %EndTag(PUBLISH)%
-
-// %Tag(SPINONCE)%
-    ros::spinOnce();
-// %EndTag(SPINONCE)%
-
-// %Tag(RATE_SLEEP)%
-    loop_rate.sleep();
-// %EndTag(RATE_SLEEP)%
-  }
-
-
-  return 0;
+	return 0;
 }
-// %EndTag(FULLTEXT)%
+
+
+
+
+
